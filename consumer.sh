3a48fb77a21ed8d7f88a0e2073ed214710c41e87
gfzhujie <gfzhujie@gf.com.cn>
2018-04-02 10:57:49 +0800

    update

---
+++ consumer.sh
@@ -0,0 +1,2 @@
+lpath=/home/gfzhujie/zhujie/kafka/kafka_2.11-1.0.0
+$lpath//bin/kafka-console-consumer.sh --zookeeper localhost:2181 --topic test --from-beginning
--- start_zookeep.sh
+++ start_zookeep.sh
@@ -1,1 +1,2 @@
-/bin/zookeeper-server-start.sh -daemon config/zookeeper.properties
+lpath=/home/gfzhujie/zhujie/kafka/kafka_2.11-1.0.0
+$lpath/bin/zookeeper-server-start.sh -daemon config/zookeeper.properties
