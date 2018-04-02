fb121779b29cba4969d8c43512890c99d53a212f
gfzhujie <gfzhujie@oa.gf.com.cn>
2018-04-02 13:46:31 +0800

    update

--- consumer.sh
+++ consumer.sh
@@ -1,3 +1,4 @@
 #lpath=/home/gfzhujie/zhujie/kafka/kafka_2.11-1.0.0
 lpath=/Users/zj/company_code/kafakainfo
-$lpath//bin/kafka-console-consumer.sh --zookeeper localhost:2181 --topic test --from-beginning
+$lpath//bin/kafka-console-consumer.sh --zookeeper localhost:2181 --topic test
+#$lpath//bin/kafka-console-consumer.sh --zookeeper localhost:2181 --topic test --from-beginning
